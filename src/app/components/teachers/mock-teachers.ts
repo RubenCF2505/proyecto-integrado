62810699f293597442deaa12d18b273ab51443be
RubenCF2505 <149149507+RubenCF2505@users.noreply.github.com>
2023-10-31 13:19:07 +0100

    #feat
    Added more teacher info and changed other things

--- src/app/components/teachers/mock-teachers.ts
+++ src/app/components/teachers/mock-teachers.ts
@@ -1,65 +1,67 @@
 import { Teacher } from "src/app/teacher";
 
 export const TEACHERS: Teacher[] = [
+
     {
         id: 1,
-        name: 'Whis',
-        description: 'Whis takes on the role of a teacher and mentor with extraordinary abilities. As an angelic being, he possesses vast knowledge and wisdom, making him an ideal instructor. With a calm and composed demeanor, Whis is the epitome of patience and serenity.',
-        powerlevel: '11',
+        name: 'Goku',
+        description: "Goku, the iconic hero of the Dragon Ball series, may not be the first character that comes to mind when thinking of teachers, but he has played a significant role as a mentor and teacher to various characters throughout the series. Goku's teaching style is rooted in his own experiences and values, making him a unique and impactful educator.",
+        classes: "Goku's teaching philosophy is deeply rooted in his own continuous quest for self-improvement. He encourages his students to always strive for greatness and never settle for mediocrity. Goku's dedication to becoming stronger serves as a powerful example to his disciples.",
+        index: "assets/images/bola.png",
         img: "assets/images/logo.png",
-        index:"assets/images/bola.png",
-        style:"Whis employs a unique and effective teaching style that combines guidance, martial arts training, and life lessons. He is a strict but fair teacher who emphasizes discipline and perseverance. He values the development of both physical and mental skills, pushing his students to reach their full potential.",
-        hability:" Whis is an unparalleled martial arts master. He imparts his knowledge of combat techniques, including the mysterious and powerful Ultra Instinct, to his students. Under his guidance, students learn to harness their energy, perfect their fighting forms, and develop superhuman abilities."
+        style: "Goku's teaching style is characterized by his hands-on, practical approach. He often believes that the best way to learn is through experience, and he encourages his students to face challenges head-on. He prefers to lead by example, showing his students what is possible through hard work, determination, and a strong sense of justice.",
+        abilities: "Goku is one of the most powerful martial artists in the Dragon Ball universe. He imparts his vast knowledge of combat techniques, such as the Kamehameha and various transformations like Super Saiyan, to his students. His combat training is intense and focused on honing their physical abilities.s"
     },
     {
         id: 2,
-        name: 'Kaio Sama',
-        description: 'Kaio-sama, or King Kai, is a beloved character from the Dragon Ball series who serves as a teacher and mentor to the Z Fighters. Despite his diminutive size and jovial personality, King Kai is a wise and experienced martial arts master who imparts invaluable knowledge to his students.',
-        powerlevel: '22',
-        index:"assets/images/bola.png",
+        name: 'Vegeta',
+        description: 'Vegeta, a character in the Dragon Ball series, is not traditionally portrayed as a teacher or mentor; however, in certain moments and contexts, he does take on the role of imparting knowledge and skills to others.',
+        classes: "Vegeta's training sessions are incredibly intense. He pushes his students to surpass their limits through grueling workouts and battles. His high expectations and competitive spirit often serve as a source of motivation for his students to excel.",
+        index: "assets/images/bola.png",
         img: "assets/images/logo.png",
-        style:" Kaio Sama has a unique and unconventional teaching style. He is known for his quirky sense of humor and laid-back demeanor, often incorporating humor into his lessons. He prefers to train in a relaxed and informal atmosphere, which helps his students feel at ease while learning.",
-        hability:"Kaio Sama is a renowned martial arts expert who specializes in techniques like the Kaio-ken and the Spirit Bomb. He shares his martial arts knowledge with his students, teaching them advanced techniques and strategies that prove crucial in battles against powerful foes."
+        style: "Vegeta's teaching style is characterized by his strict, disciplined, and no-nonsense approach. He has a strong belief in pushing his students to their limits, demanding constant improvement and perfection in their techniques. He's a tough and demanding mentor.",
+        abilities: "Vegeta is one of the most powerful Saiyans and martial artists in the Dragon Ball universe. He imparts his knowledge of combat techniques, strategies, and Saiyan transformations to his students. His training focuses on developing raw power, endurance, and combat instincts."
     },
     {
         id: 3,
-        name: 'teacher3',
-        description: 'fffffffffff',
-        powerlevel: '33',
-        index:"assets/images/bola.png",
+        name: 'Master Roshi',
+        description: 'Master Roshi, a beloved character in the Dragon Ball series, assumes the role of a teacher and mentor to several prominent characters, including Goku and Krillin. Despite his eccentricities and occasional perverted tendencies, Master Roshi is a highly respected martial arts master known for his vast experience and wisdom.',
+        classes: " Training under Master Roshi is physically demanding. He pushes his students to improve their strength, speed, and endurance through rigorous exercises and sparring sessions. His training also focuses on developing their unique strengths and abilities.",
+        index: "assets/images/bola.png",
         img: "assets/images/logo.png",
-        style:"",
-        hability:""
+        style: "Master Roshi's teaching style is unconventional yet effective. He combines traditional martial arts training with a dash of humor and quirkiness. He often uses unorthodox methods to push his students to their limits, instilling discipline and perseverance.",
+        abilities: "Master Roshi is a legendary martial artist and is the creator of the Kamehameha, a powerful energy wave technique. He imparts his knowledge of martial arts, combat techniques, and strategic thinking to his students, teaching them to harness their energy and develop their fighting skills."
     },
     {
         id: 4,
-        name: 'teacher4',
-        description: 'hhhhhhhhhhhhhh',
-        powerlevel: '44',
-        index:"assets/images/bola.png",
+        name: 'Whis',
+        description: 'Whis takes on the role of a teacher and mentor with extraordinary abilities. As an angelic being, he possesses vast knowledge and wisdom, making him an ideal instructor. With a calm and composed demeanor, Whis is the epitome of patience and serenity.',
+        classes: "Beyond combat and physical training, Whis imparts valuable life lessons to his students. He emphasizes humility, respect, and responsibility, encouraging them to be mindful of their actions and choices.",
         img: "assets/images/logo.png",
-        style:"",
-        hability:""
+        index: "assets/images/bola.png",
+        style: "Whis employs a unique and effective teaching style that combines guidance, martial arts training, and life lessons. He is a strict but fair teacher who emphasizes discipline and perseverance. He values the development of both physical and mental skills, pushing his students to reach their full potential.",
+        abilities: " Whis is an unparalleled martial arts master. He imparts his knowledge of combat techniques, including the mysterious and powerful Ultra Instinct, to his students. Under his guidance, students learn to harness their energy, perfect their fighting forms, and develop superhuman abilities."
     },
     {
         id: 5,
-        name: 'teacher5',
-        description: 'jjjjjjjjjjjjjjjjjj',
-        powerlevel: '55',
-        index:"assets/images/bola.png",
+        name: 'Kaio Sama',
+        description: 'Kaio-sama, or King Kai, is a beloved character from the Dragon Ball series who serves as a teacher and mentor to the Z Fighters. Despite his diminutive size and jovial personality, King Kai is a wise and experienced martial arts master who imparts invaluable knowledge to his students.',
+        classes:"In addition to physical combat training, King Kai emphasizes the importance of mental and spiritual development. He teaches his students to remain calm and focused under pressure, a skill that becomes invaluable during intense battles.",
+        index: "assets/images/bola.png",
         img: "assets/images/logo.png",
-        style:"",
-        hability:""
+        style: " Kaio Sama has a unique and unconventional teaching style. He is known for his quirky sense of humor and laid-back demeanor, often incorporating humor into his lessons. He prefers to train in a relaxed and informal atmosphere, which helps his students feel at ease while learning.",
+        abilities: "Kaio Sama is a renowned martial arts expert who specializes in techniques like the Kaio-ken and the Spirit Bomb. He shares his martial arts knowledge with his students, teaching them advanced techniques and strategies that prove crucial in battles against powerful foes."
     },
     {
         id: 6,
-        name: 'teacher6',
-        description: 'lllllllllllllllllllllllll',
-        powerlevel: '66',
-        index:"assets/images/bola.png",
+        name: 'Piccolo',
+        description: "Piccolo, a character from the Dragon Ball series, takes on the role of a teacher and mentor with a distinctive approach. As a Namekian warrior and one of Earth's protectors, Piccolo offers valuable guidance and martial arts training to various characters, emphasizing discipline, strength, and personal growth.",
+        classes: "Piccolo's training sessions are highly focused on developing both physical strength and mental resilience. He encourages his students to master their own weaknesses and harness their inner potential.",
+        index: "assets/images/bola.png",
         img: "assets/images/logo.png",
-        style:"",
-        hability:""
+        style: "Piccolo's teaching style is characterized by its discipline and intensity. He is a stern but fair mentor, demanding the utmost effort and dedication from his students. He believes in rigorous training and pushes his pupils to reach their full potential.",
+        abilities: " Piccolo is a formidable martial artist and strategist. He imparts his knowledge of combat techniques, such as the Special Beam Cannon and the Demon Wave, to his students. His training emphasizes precision, power, and strategic thinking in battle."
+
     },
 
 ];
--- src/app/teacher.ts
+++ src/app/teacher.ts
@@ -2,9 +2,9 @@ export interface Teacher {
   id: number,
   name: string,
   description: string,
-  powerlevel: string,
+  classes: string,
   index:string,
   img: string,
   style:string,
-  hability:string
+  abilities:string
 }
