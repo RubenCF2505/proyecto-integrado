718cf2d61fa2bd2a173f6aa8d722cd9a0cac953c
RubenCF2505 <149149507+RubenCF2505@users.noreply.github.com>
2023-11-09 19:29:29 +0100

    fixed burger menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, HostListener, OnInit, Input } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
-
 
 @Component({
   selector: 'app-header',
@@ -10,8 +10,16 @@ import { MenuItem } from 'primeng/api';
 export class HeaderComponent implements OnInit {
   items: MenuItem[] | undefined;
   imgPath = '/assets/images/logo.png';
-  progress: any
-  constructor() { }
+  progress: any;
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+  
+      
+        // Call the function to close the mobile menu
+        this.closeMobileMenu();
+      
+    });
+  }
   ngOnInit() {
     this.items = [
       { label: 'Home', id: '' },
@@ -20,7 +28,7 @@ export class HeaderComponent implements OnInit {
       { label: 'About us', id: 'aboutUs' },
       { label: 'Teacher Access', id: 'login' },
     ];
-    this.checkSize()
+    this.checkSize();
   }
   isMobile: boolean = false;
   mobileMenuOpen: boolean = false;
@@ -30,17 +38,18 @@ export class HeaderComponent implements OnInit {
   toggleMobileMenu(): void {
     this.mobileMenuOpen = !this.mobileMenuOpen;
   }
+  closeMobileMenu(): void {
+    if (this.isMobile && this.mobileMenuOpen) {
+      this.mobileMenuOpen = false;
+    }
+  }
   @HostListener('window:resize', ['$event'])
   onResize(event: any): void {
     this.isMobile = window.innerWidth <= 1024;
   }
   checkSize(): void {
-if( window.innerWidth <= 1024){
-  this.isMobile=true
-}
+    if (window.innerWidth <= 1024) {
+      this.isMobile = true;
+    }
   }
-
-
-
-
 }
