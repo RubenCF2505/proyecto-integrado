1a126615b496a93cdeea5aadf771748c20b6bc9c
RubenCF2505 <149149507+RubenCF2505@users.noreply.github.com>
2023-11-24 19:41:40 +0100

    feat
    created query to save in dataBase

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -9,129 +9,123 @@ import { DataList } from 'src/app/data-list';
 import { Validadores } from 'src/app/validadores';
 import { List } from '../../mock-students';
 import { TEACHERS } from '../teachers/mock-teachers';
+import { ServicesService } from 'src/app/services.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
-
 export class FormComponent {
-  Id = false
-  teacherId = -1
-  teachers = TEACHERS
-  community = (JSON.parse(JSON.stringify(CCAA))).default
-  DISTRICTS = JSON.parse(JSON.stringify(DISTRICTS)).default
-  CITIES = JSON.parse(JSON.stringify(CITIES)).default
-  provincias: DataList[] = []
-  ciudades: DataList[] = []
-  students = List
-  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
-
+  Id = false;
+  teacherId = -1;
+  teachers = TEACHERS;
+  community = JSON.parse(JSON.stringify(CCAA)).default;
+  DISTRICTS = JSON.parse(JSON.stringify(DISTRICTS)).default;
+  CITIES = JSON.parse(JSON.stringify(CITIES)).default;
+  provincias: DataList[] = [];
+  ciudades: DataList[] = [];
+  students = List;
+  data: any;
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private service: ServicesService
+  ) {}
 
   form = this.formBuilder.group({
+    firstName: ['', [, Validadores.validateNames]],
+    surName: ['', [, Validadores.validateNames]],
+    nif: ['', []],
+    birthDate: ['', []],
+    phone: ['', []],
 
-    firstName: ['', [Validators.required, Validadores.validateNames,]],
-    surName: ['', [Validators.required, Validadores.validateNames,]],
-    nif: ['', [Validators.required]],
-    birthDate: ['', [Validators.required]],
-    phone: ['', [Validators.required]],
-
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [, Validators.email]],
     place: this.formBuilder.group({
-      AC: ['', [Validators.required]],
-      district: [{ value: "", disabled: true }, [Validators.required]],
-      city: [{ value: "", disabled: true }, [Validators.required]]
+      AC: ['', []],
+      district: [{ value: '', disabled: true }, []],
+      city: [{ value: '', disabled: true }, []],
     }),
     address: this.formBuilder.group({
-
-      st: ['', [Validators.required]],
-      number: ['', [Validators.required, Validadores.isNumber(),]],
-      PC: ['', [Validators.required, Validadores.checkNumberLength(), Validadores.isNumber(),]],
+      st: ['', []],
+      number: ['', [, Validadores.isNumber()]],
+      PC: ['', [, Validadores.checkNumberLength(), Validadores.isNumber()]],
       floor: [''],
-      letter: ['']
+      letter: [''],
     }),
-    teacher: ['']
-
-  })
+    teacher: [''],
+  });
   addressFormGroup = this.form.get('address') as FormGroup;
 
   ngOnInit(): void {
-    this.setDistrict()
+    this.setDistrict();
 
+    this.setCity();
 
-    this.setCity()
-
-    this.getTeacher()
-    this.showImage()
+    this.getTeacher();
+    this.showImage();
   }
   setDistrict() {
-    this.form.get('place.AC')!.valueChanges.subscribe(x => {
-      this.form.get('place.district')?.enable()
-      this.provincias = []
-      this.ciudades = []
-      this.showProvinces(x!)
-    })
+    this.form.get('place.AC')!.valueChanges.subscribe((x) => {
+      this.form.get('place.district')?.enable();
+      this.provincias = [];
+      this.ciudades = [];
+      this.showProvinces(x!);
+    });
   }
   setCity() {
-    this.form.get('place.district')!.valueChanges.subscribe(x => {
-
-
-      this.form.get('place.city')?.enable()
-      this.ciudades = []
-      this.showCities(x!)
-
-    })
+    this.form.get('place.district')!.valueChanges.subscribe((x) => {
+      this.form.get('place.city')?.enable();
+      this.ciudades = [];
+      this.showCities(x!);
+    });
   }
   showProvinces(id: string) {
     this.DISTRICTS.forEach((element: DataList) => {
       if (element.parent_code == id) {
-        this.provincias.push(element)
+        this.provincias.push(element);
       }
     });
   }
   showCities(id: string) {
     this.CITIES.forEach((element: DataList) => {
       if (element.parent_code == id) {
-
-        this.ciudades.push(element)
+        this.ciudades.push(element);
       }
     });
   }
   getTeacher(): void {
-    let id: any = this.route.snapshot.paramMap.get('id')
-    id = id?.replace('teacherId=', '')
+    let id: any = this.route.snapshot.paramMap.get('id');
+    id = id?.replace('teacherId=', '');
 
     if (id != undefined) {
       this.Id = true;
-      this.teacherId = id
-      this.form.get('teacher')?.setValue(this.teachers[this.teacherId - 1].name)
-
+      this.teacherId = id;
+      this.form
+        .get('teacher')
+        ?.setValue(this.teachers[this.teacherId - 1].name);
     }
-
-
   }
-  onSubmit() {
-    let student: any = this.form.value
 
-
-    this.students.push(student)
-    console.log(this.students);
-
-  }
   setId() {
-    this.Id = false
+    this.Id = false;
   }
   showImage() {
     if (this.Id) {
-      const img = this.teachers[this.teacherId - 1].img
-      document.getElementById('img')!.style.backgroundImage = `url(${img})`
+      const img = this.teachers[this.teacherId - 1].img;
+      document.getElementById('img')!.style.backgroundImage = `url(${img})`;
     }
     this.form.get('teacher')?.valueChanges.subscribe((x: any) => {
-      const img = this.teachers[x - 1].img
-      document.getElementById('img')!.style.backgroundImage = `url(${img})`
+      const img = this.teachers[x - 1].img;
+      document.getElementById('img')!.style.backgroundImage = `url(${img})`;
     });
-
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+      this.service.saveFormData(this.form.value).subscribe((response) => {
+        console.log('Form data saved successfully:', response);
+        // Handle success as needed
+      });
+    }
+  }
 }
-
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
@@ -9,7 +9,7 @@ import { TEACHERS } from './components/teachers/mock-teachers';
   providedIn: 'root',
 })
 export class ServicesService {
-  private apiUrl = 'http://localhost/api/login.php';
+  private apiUrl = 'http://localhost/api/';
   cookie: string;
   user: string;
   items = [
@@ -17,13 +17,20 @@ export class ServicesService {
     { label: 'Matriculate', id: 'matriculate' },
     { label: 'Choose your teacher', id: 'teachers' },
     { label: 'About us', id: 'aboutUs' },
-    { label: 'Teacher Access', id: 'login' },
   ];
   constructor(
     private http: HttpClient,
     private cookieService: CookieService,
     private router: Router
-  ) {}
+  ) {
+    if (this.isLoggedIn().length > 0) {
+      this.items.push({ label: 'Alumnos', id: 'checkList' });
+    } else {
+      this.items.push({ label: 'Teacher Access', id: 'login' });
+    }
+  }
+
+
 
   getTeacherById(id: number): Observable<string> {
     const url = `/matriculate/${id}`;
@@ -42,7 +49,7 @@ export class ServicesService {
     formData.append('username', username);
     formData.append('password', password);
 
-    this.http.post(this.apiUrl, formData).subscribe((response: any) => {
+    this.http.post(this.apiUrl+'login.php', formData).subscribe((response: any) => {
       if (response.status === 'success') {
         this.user = username;
         this.cookieService.set('cookie', response.token);
@@ -52,17 +59,25 @@ export class ServicesService {
       }
     });
   }
+
+
+  saveFormData(formData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}add.php  `, formData);
+  }
+  
   hideHeaderOnSpecificRoute() {
     return this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd)
     );
   }
-  
+
   isLoggedIn(): string {
     return this.cookieService.get('cookie');
   }
   logout() {
     this.cookieService.delete('cookie');
+    this.items[4].label="Teacher Access"
+    this.items[4].id="login"
   }
   getUser() {
     return this.user;
