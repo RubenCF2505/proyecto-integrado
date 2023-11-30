cfcb2ed3d42c7124eb7266f146436ae4e65c69ad
RubenCF2505 <149149507+RubenCF2505@users.noreply.github.com>
2023-11-30 22:24:06 +0100

    feat(rD): created table to show students

--- src/app/components/check-list/check-list.component.ts
+++ src/app/components/check-list/check-list.component.ts
@@ -5,34 +5,51 @@ import { Student } from 'src/app/student';
 @Component({
   selector: 'app-check-list',
   templateUrl: './check-list.component.html',
-  styleUrls: ['./check-list.component.css']
+  styleUrls: ['./check-list.component.css'],
 })
 export class CheckListComponent {
-  teachers = TEACHERS
-  constructor(private service: ServicesService) { }
+  teachers = TEACHERS;
+  constructor(private service: ServicesService) {}
   selectedItemId: number | null = null; // Initialize as null or any default value
-  savedStudents:string[]=[]
-  keys:string[]
-ngOnInit(){
-  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-  //Add 'implements OnInit' to the class.
-  this.service.getStudents().subscribe((data:any)=>{
-    this.service.students=data
-    console.log(data);
-    
-    this.keys=Object.keys(data[0])
-    this.savedStudents=data
-  })
+  savedStudents: any[] = [];
+  keys: string[] = [];
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.service.getStudents().subscribe((data: any) => {
+      this.service.students = data;
+      this.keys = Object.keys(data[0]);
 
-  
-  
-}
+      data.forEach((element:any)=>{
+ //element es lo que hay que pasar a array
+        this.savedStudents.push(Object.values(element))
+      
+      })
+    });
+  }
+
+
+
+  // Ordenar
+
+  // Listar con el nuevo order
+
+  // Call the function to get the sorted array
+
+  sortObject(inputObject: any, order: string[]): any {
+    const sortedObject: any = {};
+    order.forEach((key) => {
+      if (inputObject.hasOwnProperty(key)) {
+        sortedObject[key] = inputObject[key];
+      }
+    });
+    return sortedObject;
+  }
   // Function to handle item click
   handleItemClick(itemId: number) {
     this.selectedItemId = itemId; // Set the selected item ID when clicked
   }
   onLogout() {
-    this.service.logout()
+    this.service.logout();
   }
-  
 }
